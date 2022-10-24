a3cdc1d21fabbde6ff80cb6730402263cd97fe66
davigarms <davigarms@gmail.com>
2022-10-24 18:30:37 +0100

    Add PasswordValidator interface

---
+++ PasswordValidation/IValidationRules.cs
@@ -0,0 +1,40 @@
+﻿namespace PasswordValidation;
+
+public interface IValidationRules
+{
+    public bool IsValid(string password);
+}
+
+public class ValidationRuleOne : IValidationRules
+{
+    public bool IsValid(string password)
+    {
+        return password.Length > 8 &&
+               password.Any(char.IsUpper) &&
+               password.Any(char.IsLower) &&
+               password.Any(char.IsNumber) &&
+               password.Any(x => x.Equals('_'));
+    }
+}
+
+public class ValidationRuleTwo : IValidationRules
+{
+    public bool IsValid(string password)
+    {
+        return password.Length > 6 &&
+               password.Any(char.IsUpper) &&
+               password.Any(char.IsLower) &&
+               password.Any(char.IsNumber);
+    }
+}
+
+public class ValidationRuleThree : IValidationRules
+{
+    public bool IsValid(string password)
+    {
+        return password.Length > 16 &&
+               password.Any(char.IsUpper) &&
+               password.Any(char.IsLower) &&
+               password.Any(x => x.Equals('_'));
+    }
+}
---
+++ PasswordValidation/PasswordValidator.cs
@@ -0,0 +1,16 @@
+﻿namespace PasswordValidation;
+
+public class PasswordValidator
+{
+    private readonly IValidationRules _validationRule;
+
+    public PasswordValidator(IValidationRules validationRule)
+    {
+        _validationRule = validationRule;
+    }
+
+    public bool ValidatePassword(string password)
+    {
+        return _validationRule.IsValid(password);
+    }
+}
