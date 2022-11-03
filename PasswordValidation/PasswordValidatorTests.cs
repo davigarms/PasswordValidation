7558df1803bacbe81e54290adf3ceae6d5c52030
davigarms <davigarms@gmail.com>
2022-11-03 17:40:31 +0000

    Add ValidatorRules class

--- PasswordValidation/PasswordValidatorTests.cs
+++ PasswordValidation/PasswordValidatorTests.cs
@@ -1,4 +1,6 @@
+using System.Data;
 using FakeItEasy;
+using NUnit.Framework.Constraints;
 
 namespace PasswordValidation;
 
@@ -12,7 +14,7 @@ public class PasswordValidatorTests
     public void ValidatePassword_rule_one_returns_true_when_password_is_valid(string password)
     {
         var validator = new PasswordValidator(new ValidationRuleOneFactory());
-        var isValid =  validator.IsValid(password);
+        var isValid = validator.IsValid(password);
         Assert.That(isValid, Is.True);
     }
 
@@ -23,7 +25,7 @@ public class PasswordValidatorTests
     public void ValidatePassword_rule_two_returns_true_when_password_is_valid(string password)
     {
         var validator = new PasswordValidator(new ValidationRuleTwoFactory());
-        var isValid =  validator.IsValid(password);
+        var isValid = validator.IsValid(password);
         Assert.That(isValid, Is.True);
     }
 
@@ -34,7 +36,7 @@ public class PasswordValidatorTests
     public void ValidatePassword_rule_three_returns_true_when_password_is_valid(string password)
     {
         var validator = new PasswordValidator(new ValidationRuleThreeFactory());
-        var isValid =  validator.IsValid(password);
+        var isValid = validator.IsValid(password);
         Assert.That(isValid, Is.True);
     }
 
@@ -55,4 +57,75 @@ public class PasswordValidatorTests
     {
         Assert.IsInstanceOf<ValidationRuleOne>(new ValidationRuleOneFactory().Create());
     }
+
+    [TestCase(8, "validpassword", true)]
+    [TestCase(16, "invalidpassword", false)]
+    public void LenghtRule_returns_expected_value(int minLength, string password, bool expected)
+    {
+        var rule = new LenghtValidationRule(minLength);
+        Assert.That(rule.Validate(password), Is.EqualTo(expected));
+    }
+
+    [TestCase("Validpassword", true)]
+    [TestCase("invalidpassword", false)]
+    public void UpperCaseRule_returns_expected_value(string password, bool expected)
+    {
+        var rule = new UpperCaseRule();
+        Assert.That(rule.Validate(password), Is.EqualTo(expected));
+    }
+    
+    [Test]
+    public void RuleValidator_add_a_new_rule()
+    {
+        var ruleValidator = new RuleValidator();
+        ruleValidator.Add(new LenghtValidationRule(8));
+        Assert.That(ruleValidator.RuleCount, Is.EqualTo(1));
+    }
+
+    [TestCase(8, "validpassword", true)]
+    [TestCase(16, "invalidpassword", false)]
+    public void RuleValidator_with_LengthRule_return_expected(int minLenght, string password, bool expected)
+    {
+        var ruleValidator = new RuleValidator();
+        ruleValidator.Add(new LenghtValidationRule(minLenght));
+        Assert.That(ruleValidator.IsValid(password), Is.EqualTo(expected));
+    }
+}
+
+public class RuleValidator
+{
+    private readonly List<IRule> _rules;
+
+    public RuleValidator()
+    {
+        _rules = new List<IRule>();
+    }
+
+    public bool IsValid(string password) => _rules.All(rule => rule.Validate(password));
+
+    public int RuleCount => _rules.Count;
+
+    public void Add(IRule rule) => _rules.Add(rule);
+}
+
+public interface IRule
+{
+    public bool Validate(string password);
+}
+
+public class UpperCaseRule
+{
+    public bool Validate(string password) => password.Any(char.IsUpper);
+}
+
+public class LenghtValidationRule : IRule
+{
+    private readonly int _length;
+
+    public LenghtValidationRule(int length)
+    {
+        _length = length;
+    }
+
+    public bool Validate(string password) => password.Length > _length;
 }
